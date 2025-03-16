159fe4c285177486ec634bd4ce124ba15fd954a5
ProBonoEngineeringServicesAdvice <mariavasq@proton.me>
2025-03-16 12:34:14 -0400

    Update script.js
    Signed-off-by: ProBonoEngineeringServicesAdvice <mariavasq@proton.me>

--- script.js
+++ script.js
@@ -33,5 +33,17 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
         alert("Thank you for reaching out! We will get back to you soon.");
         this.reset();
+        document.addEventListener("DOMContentLoaded", function () {
+    let hoverContainer = document.querySelector(".hover-container");
+    let hoverContent = document.querySelector(".hover-content");
+
+    hoverContainer.addEventListener("mouseover", function () {
+        hoverContent.style.display = "block";
+    });
+
+    hoverContainer.addEventListener("mouseout", function () {
+        hoverContent.style.display = "none";
+    });
+});
     });
 });
